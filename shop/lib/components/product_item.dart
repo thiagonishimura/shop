73a7fcddf9a9bc6d115193cec6f02b21b9abb0db
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-12 16:27:45 -0300

    Usando Provider
    Utilizando Provider para acessar e notificar as paginas.

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/models/product_detail_page.dart';
 import 'package:shop/utils/app_routers.dart';
 
 class ProductItem extends StatelessWidget {
--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop/models/product_detail_page.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routers.dart';
 
@@ -12,20 +14,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRouters.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRouters.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
