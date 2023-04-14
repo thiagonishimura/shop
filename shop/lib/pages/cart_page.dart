5b5de0c8c9fc02a14052de2e0379b50b6c31c1e6
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-14 15:48:29 -0300

    Add ao carrinho
    Adicionando itens no carrinho

--- shop/lib/pages/cart_page.dart
+++ shop/lib/pages/cart_page.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
+      ),
+      body: Card(
+        margin: EdgeInsets.all(25),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Total',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(width: 10),
+              Chip(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                label: Text(
+                  'R\$${cart.totalAmount.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Spacer(),
+              TextButton(
+                child: Text('COMPRAR'),
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
