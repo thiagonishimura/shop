27d7d0f4d6d5bc508719ffafc0d84c34ff18daf5
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-15 08:54:20 -0300

    Item da lista de compras
    Item da lista de compras adicionados e funcionando

--- shop/lib/components/cart_item.dart
+++ shop/lib/components/cart_item.dart
@@ -11,6 +11,30 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: EdgeInsetsDirectional.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('${cartItem.price}'),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text(
+              'Total R\$ ${(cartItem.price * cartItem.quantity).toStringAsFixed(2)}'),
+          trailing: Text('${cartItem.quantity}X'),
+        ),
+      ),
+    );
   }
 }
