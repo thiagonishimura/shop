e7dab6d8eedbf057a473519dcad14addd7c47bed
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-18 14:49:22 -0300

    Formulario produtos
    Formulário de produtos realizado.

--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -16,6 +16,30 @@ class _ProductFormPageState extends State<ProductFormPage> {
       appBar: AppBar(
         title: Text('Formulário de produto'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                textInputAction: TextInputAction.next,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Descrição'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
