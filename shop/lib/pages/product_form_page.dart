30502dabf2e8392917a327944964b3aa7d988475
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-18 16:47:00 -0300

    Submit formulario
    Formulario submit ok

--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -1,16 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 
 class ProductFormPage extends StatefulWidget {
-  const ProductFormPage({super.key});
+  const ProductFormPage({Key? key}) : super(key: key);
 
   @override
   State<ProductFormPage> createState() => _ProductFormPageState();
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
-  final _imageUrlController = TextEditingController();
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
   final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  final _formData = <String, Object>{};
 
   @override
   void initState() {
@@ -21,57 +29,100 @@ class _ProductFormPageState extends State<ProductFormPage> {
   @override
   void dispose() {
     super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+
     _imageUrlFocus.removeListener(updateImage);
-    _imageUrlController.dispose();
+    _imageUrlFocus.dispose();
   }
 
   void updateImage() {
     setState(() {});
   }
 
+  void _submitForm() {
+    _formKey.currentState?.save();
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      name: _formData['name'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+    print(newProduct.id);
+    print(newProduct.name);
+    print(newProduct.description);
+    print(newProduct.price);
+    print(newProduct.imageUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Product product;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Formulário de produto'),
+        title: const Text('Formulário de Produto'),
+        actions: [
+          IconButton(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.save),
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Nome'),
+                decoration: const InputDecoration(labelText: 'Nome'),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
+                onSaved: (name) => _formData['name'] = name ?? '',
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Preço'),
+                decoration: const InputDecoration(labelText: 'Preço'),
                 textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                focusNode: _priceFocus,
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                },
+                onSaved: (price) =>
+                    _formData['price'] = double.parse(price ?? '0'),
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Descrição'),
-                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(labelText: 'Descrição'),
+                focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                onSaved: (description) =>
+                    _formData['description'] = description ?? '',
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
                     child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Url da imagem'),
-                      textInputAction: TextInputAction.none,
+                      decoration:
+                          const InputDecoration(labelText: 'Url da Imagem'),
                       keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
                       focusNode: _imageUrlFocus,
                       controller: _imageUrlController,
+                      onFieldSubmitted: (_) => _submitForm(),
+                      onSaved: (imageUrl) =>
+                          _formData['imageUrl'] = imageUrl ?? '',
                     ),
                   ),
                   Container(
-                    width: 100,
                     height: 100,
-                    margin: EdgeInsets.only(
+                    width: 100,
+                    margin: const EdgeInsets.only(
                       top: 10,
                       left: 10,
                     ),
@@ -83,10 +134,10 @@ class _ProductFormPageState extends State<ProductFormPage> {
                     ),
                     alignment: Alignment.center,
                     child: _imageUrlController.text.isEmpty
-                        ? Text('Informe a Url')
+                        ? const Text('Informe a Url')
                         : FittedBox(
-                            child: Image.network(_imageUrlController.text),
                             fit: BoxFit.cover,
+                            child: Image.network(_imageUrlController.text),
                           ),
                   ),
                 ],
