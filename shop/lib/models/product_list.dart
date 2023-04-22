9062045779f6e680e9acdb21fc48aca401651c72
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-22 09:31:06 -0300

    get Product do BD.

--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:shop/data/dummy_data.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final List<Product> _items = dummyProducts;
-  final _baseUrl = 'https://shop-nisimura-default-rtdb.firebaseio.com';
+  final _url =
+      'https://shop-nisimura-default-rtdb.firebaseio.com/products.json';
+  final List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -37,8 +37,8 @@ class ProductList with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    final response = await post(
-      Uri.parse('$_baseUrl/products.json'),
+    final response = await http.post(
+      Uri.parse(_url),
       body: jsonEncode(
         {
           "name": product.name,
@@ -60,6 +60,25 @@ class ProductList with ChangeNotifier {
       isFavorite: product.isFavorite,
     ));
 
+    notifyListeners();
+  }
+
+  Future<void> loadProducts() async {
+    final response = await http.get(Uri.parse(_url));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          name: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
     notifyListeners();
   }
 
