43b7c0e74023fec072b89ab60ffc0d500cebf848
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-24 14:15:03 -0300

    Obtendo Pedidos

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   static const PRODUCT_BASE_URL =
       'https://shop-nisimura-default-rtdb.firebaseio.com/products';
   static const ORDER_BASE_URL =
-      'https://shop-nisimura-default-rtdb.firebaseio.com/order';
+      'https://shop-nisimura-default-rtdb.firebaseio.com/orders';
 }
